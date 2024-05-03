7661968eb306cd87b4b7087bacc89683e2093254
Andrea Peralta <andreacpb90@hotmail.com>
2024-05-03 16:53:15 -0500

    Adding dependencies and model classes for users and roles

---
+++ osiris-backend/src/main/java/com/osiris/backend/model/Role.java
@@ -0,0 +1,20 @@
+package com.osiris.backend.model;
+
+import jakarta.persistence.*;
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
