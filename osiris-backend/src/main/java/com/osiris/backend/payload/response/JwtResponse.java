d6a38edef138ed32d4df3b91dd97842c0d111cee
Andrea Peralta <andreacpb90@hotmail.com>
2024-05-04 15:28:26 -0500

    Refresh Token Request and Response

--- osiris-backend/src/main/java/com/osiris/backend/payload/response/JwtResponse.java
+++ osiris-backend/src/main/java/com/osiris/backend/payload/response/JwtResponse.java
@@ -3,52 +3,63 @@ package com.osiris.backend.payload.response;
 import java.util.List;
 
 public class JwtResponse {
-  private String token;
-  private String type = "Bearer";
-  private Long id;
-  private String username;
-  private List<String> roles;
+	private String token;
+	private String type = "Bearer";
+	private String refreshToken;
+	private Long id;
+	private String username;
 
-  public JwtResponse(String accessToken, Long id, String username, List<String> roles) {
-    this.token = accessToken;
-    this.id = id;
-    this.username = username;
-    this.roles = roles;
+	private List<String> roles;
+
+	public JwtResponse(String accessToken, String refreshToken, Long id, String username, List<String> roles) {
+		this.token = accessToken;
+		this.refreshToken = refreshToken;
+		this.id = id;
+		this.username = username;
+		this.roles = roles;
+	}
+
+	public String getAccessToken() {
+		return token;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.token = accessToken;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+  public String getRefreshToken() {
+    return refreshToken;
   }
 
-  public String getAccessToken() {
-    return token;
-  }
-
-  public void setAccessToken(String accessToken) {
-    this.token = accessToken;
-  }
-
-  public String getTokenType() {
-    return type;
-  }
-
-  public void setTokenType(String tokenType) {
-    this.type = tokenType;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public List<String> getRoles() {
-    return roles;
+  public void setRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
   }
 }
