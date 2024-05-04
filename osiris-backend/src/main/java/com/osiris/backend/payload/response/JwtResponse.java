b9d2e662af53b81d1005a0ea89783ab7f9c0adc8
Andrea Peralta <andreacpb90@hotmail.com>
2024-05-04 14:52:15 -0500

    Fixing issues

--- osiris-backend/src/main/java/com/osiris/backend/payload/response/JwtResponse.java
+++ osiris-backend/src/main/java/com/osiris/backend/payload/response/JwtResponse.java
@@ -7,7 +7,6 @@ public class JwtResponse {
   private String type = "Bearer";
   private Long id;
   private String username;
-  private String email;
   private List<String> roles;
 
   public JwtResponse(String accessToken, Long id, String username, List<String> roles) {
